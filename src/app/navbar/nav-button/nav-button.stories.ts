13283f15b773f753f9ddbaee33e057d9edac2efa
Yash Agarwal <yagarwal1307@gmail.com>
2021-05-29 15:21:47 +0530

    Add action for navbutton

--- src/app/navbar/nav-button/nav-button.stories.ts
+++ src/app/navbar/nav-button/nav-button.stories.ts
@@ -2,6 +2,7 @@ import { moduleMetadata } from '@storybook/angular'
 import { Meta, Story} from '@storybook/angular/types-6-0';
 import { NavButtonComponent } from './nav-button.component';
 import { CommonModule } from '@angular/common';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'MyWebsite/Components/Navbar/Navbutton',
@@ -10,11 +11,16 @@ export default {
     navItem: {
       control: {type: 'object'}
     }
-  }
+  },
+  excludeStories: /.*Data$/
 } as Meta;
 
+export const actionsData = {
+  clickEvent: action('clickEvent')
+}
+
 const Template: Story<NavButtonComponent> = (args: NavButtonComponent) => ({
-  props: args,
+  props: {...args, clickEvent: actionsData.clickEvent},
   decorators: [
     moduleMetadata({
       imports: [CommonModule]
